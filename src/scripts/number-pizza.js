c70e0fab45faf74325e15152ec6a7dfcbeac28ad
ValouZ <debray.va@gmail.com>
2021-03-15 23:30:29 +0100

    changement du prix selon la taille et gestion quantité

---
+++ src/scripts/number-pizza.js
@@ -0,0 +1,14 @@
+let removeButton = document.getElementById("app-remove");
+let addButton = document.getElementById("app-add");
+let quantity = document.getElementById("app-quantity");
+
+let counter = parseInt(quantity.textContent);
+
+addButton.addEventListener("click", () => changeQuantity(1));
+removeButton.addEventListener("click", () => changeQuantity(-1));
+
+function changeQuantity(value) {
+  counter += value;
+  counter = counter < 0 ? 0 : counter;
+  quantity.textContent = counter;
+}
--- src/scripts/pizza-size.js
+++ src/scripts/pizza-size.js
@@ -1,4 +1,7 @@
-var pizzaSizeButtons = document.getElementById("app-pizza-size").children;
+let pizzaSizeButtons = document.getElementById("app-pizza-size").children;
+let price = document.getElementById("app-price");
+
+let tabPrice = ["12.00", "13.80", "14.90"];
 
 for (let i = 0; i < pizzaSizeButtons.length; i++) {
   pizzaSizeButtons[i].addEventListener("click", chooseSize);
@@ -9,4 +12,15 @@ function chooseSize() {
     pizzaSizeButtons[i].classList.remove("active");
   }
   this.classList.add("active");
+  changePrice(this.textContent);
+}
+
+function changePrice(value) {
+  if (value == "S") {
+    price.innerHTML = tabPrice[0] + "<span> € </span>";
+  } else if (value == "M") {
+    price.innerHTML = tabPrice[1] + "<span> € </span>";
+  } else {
+    price.innerHTML = tabPrice[2] + "<span> € </span>";
+  }
 }
