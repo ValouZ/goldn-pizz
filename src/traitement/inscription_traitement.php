d13adf1d69a2c50c42b6d565a6d36291d3823333
ValouZ <debray.va@gmail.com>
2021-03-26 15:47:48 +0100

    formatage

--- src/traitement/inscription_traitement.php
+++ src/traitement/inscription_traitement.php
@@ -2,10 +2,10 @@
 
 include('pdo.php');
 
-if(isset($_POST['forminscription'])){
+if (isset($_POST['forminscription'])) {
   $pseudo = htmlspecialchars($_POST['pseudo']);
   $civilite = htmlspecialchars($_POST['civilite']);
-  $password = password_hash($_POST['password'],PASSWORD_DEFAULT);
+  $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
   $passwordConfirm = htmlspecialchars($_POST['password-confirm']);
   var_dump($passwordConfirm);
   $email = htmlspecialchars($_POST['email']);
@@ -16,26 +16,26 @@ if(isset($_POST['forminscription'])){
   $telephone = htmlspecialchars($_POST['telephone']);
 
 
-  if(!empty($_POST['pseudo']) AND !empty($_POST['civilite']) AND !empty($_POST['password']) AND !empty($_POST['password-confirm']) AND !empty($_POST['email']) AND !empty($_POST['pays'])AND !empty($_POST['postcode']) AND !empty($_POST['ville']) AND !empty($_POST['rue']) AND !empty($_POST['telephone'])){
+  if (!empty($_POST['pseudo']) and !empty($_POST['civilite']) and !empty($_POST['password']) and !empty($_POST['password-confirm']) and !empty($_POST['email']) and !empty($_POST['pays']) and !empty($_POST['postcode']) and !empty($_POST['ville']) and !empty($_POST['rue']) and !empty($_POST['telephone'])) {
     $pseudoLenght = strlen($pseudo);
-    $reqPseudo = $bdd -> prepare("SELECT * FROM client WHERE pseudo_client  = ?");
+    $reqPseudo = $bdd->prepare("SELECT * FROM client WHERE pseudo_client  = ?");
     $reqPseudo->execute(array($pseudo));
     $pseudoExist = $reqPseudo->rowCount();
-    if($pseudoExist == 0){
-      if($pseudoLenght < 18){
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+    if ($pseudoExist == 0) {
+      if ($pseudoLenght < 18) {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
           $reqemail = $bdd->prepare("SELECT * FROM client WHERE email_client = ?");
           $reqemail->execute(array($email));
           $mailexist = $reqemail->rowCount();
-          if($mailexist == 0){
-            if(preg_match('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%&(){}[]:;<>,.?/~_+-=|\]).10,20}$^', $password) !== 1){
-              if(password_verify($passwordConfirm, $password)){
-                if(filter_var(intval($telephone), FILTER_VALIDATE_INT)){
+          if ($mailexist == 0) {
+            if (preg_match('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%&(){}[]:;<>,.?/~_+-=|\]).10,20}$^', $password) !== 1) {
+              if (password_verify($passwordConfirm, $password)) {
+                if (filter_var(intval($telephone), FILTER_VALIDATE_INT)) {
                   $telephoneLenght = strlen($telephone);
-                  if($telephoneLenght == 10){
-                    if(filter_var(intval($postcode), FILTER_VALIDATE_INT)){
+                  if ($telephoneLenght == 10) {
+                    if (filter_var(intval($postcode), FILTER_VALIDATE_INT)) {
                       $reqinsert = $bdd->prepare('INSERT INTO client(pseudo_client,email_client,mdp_client,genre_client,pays_client,ville_client,postcode_client,rue_client,tel_client,role_client) VALUES (?,?,?,?,?,?,?,?,?,0)');
-                      $reqinsert->execute(array($pseudo,$email,$password,$civilite,$pays,$ville,$postcode,$rue,$telephone));
+                      $reqinsert->execute(array($pseudo, $email, $password, $civilite, $pays, $ville, $postcode, $rue, $telephone));
                       echo "bien insere";
                     }
                   }
@@ -47,10 +47,6 @@ if(isset($_POST['forminscription'])){
       }
     }
   }
-  
-}else{
-  echo'pas bon'; // A MODIFIER
+} else {
+  echo 'pas bon'; // A MODIFIER
 }
-
-
-
