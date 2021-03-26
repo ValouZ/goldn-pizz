c9d7f3c9ad08d565fd029d74349797f4138ccc59
ValouZ <debray.va@gmail.com>
2021-03-26 17:18:56 +0100

    erreur mdp connexion

--- src/traitement/connexion-traitement.php
+++ src/traitement/connexion-traitement.php
@@ -2,12 +2,35 @@
 
 include('pdo.php');
 
-var_dump($_POST);
+// var_dump($_POST);
 
-if (isset($_POST['formconnexion'])){
+if (isset($_POST['formconnexion'])) {
   $pseudo = htmlspecialchars($_POST['pseudo']);
   $password = htmlspecialchars($_POST['password']);
 
-  echo $pseudo;
-  echo $password;
+  if (!empty($_POST['pseudo']) and !empty($_POST['password'])) {
+    $reqPseudo = $bdd->prepare("SELECT * FROM client WHERE pseudo_client  = ?");
+    $reqPseudo->execute(array($pseudo));
+    $pseudoExist = $reqPseudo->rowCount();
+    if ($pseudoExist == 1) {
+      $user = $reqPseudo->fetch(PDO::FETCH_ASSOC);
+      $passwordUser = $user['mdp_client'];
+      echo $password . '<br>';
+      var_dump(password_verify($password, $passwordUser));
+      if (password_verify($password, $passwordUser)) {
+        // if ($password === $passwordUser) {
+        echo "<h2> TU ES MAINTENANT CONNECTÉ </h2>";
+      } else {
+        echo "<h2> MAUVAIS MDP </h2> <p>" . $password . "</p><p>" . $passwordUser . "</p>";
+      }
+    } else {
+      echo "<h2> PSEUDO INCONNU </h2>";
+    }
+  } else {
+    echo "<h2> CHAMPS VIDE </h2>";
+  }
+} else {
+  echo "<h2> RIEN N'EXISTE </h2>";
 }
+?>
+<a href="../sign-in.php">RETOUR</a>
--- src/traitement/inscription-traitement.php
+++ src/traitement/inscription-traitement.php
@@ -5,9 +5,9 @@ include('pdo.php');
 if (isset($_POST['forminscription'])) {
   $pseudo = htmlspecialchars($_POST['pseudo']);
   $civilite = htmlspecialchars($_POST['civilite']);
-  $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+  // $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+  $password = htmlspecialchars($_POST['password']);
   $passwordConfirm = htmlspecialchars($_POST['password-confirm']);
-  var_dump($passwordConfirm);
   $email = htmlspecialchars($_POST['email']);
   $pays = htmlspecialchars($_POST['pays']);
   $postcode = htmlspecialchars($_POST['postcode']);
@@ -28,25 +28,46 @@ if (isset($_POST['forminscription'])) {
           $reqemail->execute(array($email));
           $mailexist = $reqemail->rowCount();
           if ($mailexist == 0) {
-            if (preg_match('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%&(){}[]:;<>,.?/~_+-=|\]).10,20}$^', $password) !== 1) {
-              if (password_verify($passwordConfirm, $password)) {
+            if (preg_match('~^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[$%?!]).{10,20}$~u', $password) == 1) {
+              if ($passwordConfirm === $password) {
                 if (filter_var(intval($telephone), FILTER_VALIDATE_INT)) {
                   $telephoneLenght = strlen($telephone);
                   if ($telephoneLenght == 10) {
                     if (filter_var(intval($postcode), FILTER_VALIDATE_INT)) {
+                      $passwordHash = password_hash($password, PASSWORD_DEFAULT);
                       $reqinsert = $bdd->prepare('INSERT INTO client(pseudo_client,email_client,mdp_client,genre_client,pays_client,ville_client,postcode_client,rue_client,tel_client,role_client) VALUES (?,?,?,?,?,?,?,?,?,0)');
-                      $reqinsert->execute(array($pseudo, $email, $password, $civilite, $pays, $ville, $postcode, $rue, $telephone));
-                      echo "bien insere";
+                      $reqinsert->execute(array($pseudo, $email, $passwordHash, $civilite, $pays, $ville, $postcode, $rue, $telephone));
+                      echo "NOUVEAU CLIENT";
+                    } else {
+                      echo "<h2> CODE POSTAL PAS CHIFFRE </h2>" . $postcode;
                     }
+                  } else {
+                    echo "<h2> LONGUEUR TELEPHONE != 10 </h2>" . $telephoneLenght;
                   }
+                } else {
+                  echo "<h2> MAUVAIS TELEPHONE </h2>" . $telephone;
                 }
+              } else {
+                echo "<h2> LES DEUX MOTS DE PASSES SONT DIFFERENTS </h2>" . $passwordConfirm . $password;
               }
+            } else {
+              echo "<h2> MAUVAIS MDP </h2>" . $passwordConfirm . $password;
             }
+          } else {
+            echo "<h2> EMAIL DEJA EXISTANT </h2>";
           }
+        } else {
+          echo "<h2> PB EMAIL </h2>";
         }
+      } else {
+        echo "<h2> PSEUDO TROP LONG </h2>" . $pseudoLenght;
       }
+    } else {
+      echo "<h2> PSEUDO EXISTE DEJA </h2>" . $pseudo;
     }
   }
 } else {
   echo 'pas bon'; // A MODIFIER
 }
+?>
+<a href="../sign-up.php">RETOUR</a>
