1e734acf690c80587648a3a9351ac5773a49fd55
ValouZ <debray.va@gmail.com>
2021-04-11 01:13:22 +0200

    new header.php and new logo

--- src/header.php
+++ src/header.php
@@ -15,62 +15,40 @@ session_start();
 </head>
 
 <body>
+  <header class="menu">
+    <nav>
+      <ul class="list-items">
+        <?php
+        if (isset($_SESSION['id']) && isset($_SESSION['pseudo'])) {
+          if ($menu == "1") {
+        ?>
+            <li class="home"><a href="index.php"><img src="assets/images/home.svg" alt="Accueil"></a></li>
+            <li><a href="logout.php">Deconnexion</a></li>
+      </ul>
+    <?php
+          } elseif ($menu == "2") { // Menu général en mode client
+    ?>
+      <li class="home"><a href="index.php"><img src="assets/images/home.svg" alt="Accueil"></a></li>
+      <li class="basket"><a href="basket.php"><img src="assets/images/basket.svg" alt="Panier"></a></li>
+      <li class="profil"><a href="profile.php"><img src="assets/images/user.svg" alt="Profil"></a></li>
+      <li class="logout"><a href="logout.php"><img src="assets/images/logout.svg" alt="Déconnexion"></a></li>
+    <?php
+          }
+        } else {
+          if ($menu == "1") { // Menu page de connexion et inscription
+    ?>
+      <li class="home"><a href="index.php"><img src="assets/images/home.svg" alt="Accueil"></a></li>
+    <?php
+          } elseif ($menu == "2") { // Menu général en mode visiteur
+    ?>
+      <li class="home"><a href="index.php"><img src="assets/images/home.svg" alt="Accueil"></a></li>
+      <li><a href="sign-in.php">Connexion</a></li>
+      <li><a href="sign-up.php">Inscription</a></li>
   <?php
-  if (isset($_SESSION['id']) && isset($_SESSION['pseudo'])) {
-
-
-    if ($menu == "1") {
+          }
+        }
   ?>
-      <header class="menu">
-        <nav>
-          <ul>
-            <li class="home"><a href="index.php"><img src="assets/images/home.svg" alt="Accueil"></a></li>
-            <li><a href="sign-out.php">Deconnexion</a></li>
-          </ul>
-        </nav>
-      </header>
-    <?php
-    } elseif ($menu == "2") {
-      // Menu général en mode client
-    ?>
-      <header class="menu">
-        <nav>
-          <ul>
-            <li class="home"><a href="index.php"><img src="assets/images/home.svg" alt="Accueil"></a></li>
-            <li><a href="basket.php">Panier</a></li>
-            <li><a href="profile.php">Profil</a></li>
-            <li><a href="sign-out.php">Deconnexion</a></li>
-          </ul>
-        </nav>
-      </header>
-    <?php
-    }
-  } else {
-    if ($menu == "1") {
-      // Menu page de connexion et inscription
-    ?>
-      <header class="menu">
-        <nav>
-          <ul>
-            <li class="home"><a href="index.php"><img src="assets/images/home.svg" alt="Accueil"></a></li>
-          </ul>
-        </nav>
-      </header>
-    <?php
-    } elseif ($menu == "2") {
-      // Menu général en mode visiteur
-    ?>
-      <header class="menu">
-        <nav>
-          <ul>
-            <li class="home"><a href="index.php"><img src="assets/images/home.svg" alt="Accueil"></a></li>
-            <li><a href="sign-in.php">Connexion</a></li>
-            <li><a href="sign-up.php">Inscription</a></li>
-          </ul>
-        </nav>
-      </header>
-  <?php
-    }
-  }
-  ?>
+  </ul>
+    </nav>
+  </header>
   <h1 class="goldn-pizz"><a href="index.php">Goldn Pizz'</a></h1>
