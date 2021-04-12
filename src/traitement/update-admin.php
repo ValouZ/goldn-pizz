b8f75cdc7b87271e93c8aaa70a86ee420b01d1b7
ValouZ <debray.va@gmail.com>
2021-04-12 23:47:06 +0200

    indentation CONNARD !

--- src/traitement/update-admin.php
+++ src/traitement/update-admin.php
@@ -3,19 +3,19 @@
 session_start();
 include('pdo.php');
 
-if(isset($_POST['validate'])){
+if (isset($_POST['validate'])) {
 
   $nomPizza = htmlspecialchars($_POST['nom-pizza']);
   $prixPizza = htmlspecialchars($_POST['prix-pizza']);
 
-  if(!empty($_POST['nom-pizza']) OR !empty($_POST['prix-pizza'])){
-    if(filter_var($prixPizza, FILTER_VALIDATE_FLOAT)){
-      $req = $bdd->prepare('UPDATE pizza SET nom_pizza = ?,prix_pizza=?');
-      $req->execute(array($nomPizza,$prixPizza));
+  if (!empty($_POST['nom-pizza']) or !empty($_POST['prix-pizza'])) {
+    if (filter_var($prixPizza, FILTER_VALIDATE_FLOAT)) {
+      $req = $bdd->prepare('UPDATE pizza SET nom_pizza = ?, prix_pizza = ?');
+      $req->execute(array($nomPizza, $prixPizza));
       header('location:../admin.php');
       exit();
     }
   }
-}else{
+} else {
   echo "caca";
 }
