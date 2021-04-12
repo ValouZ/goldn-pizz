02d9255851c48161ec3c1fac2008faa73261472b
ValouZ <debray.va@gmail.com>
2021-04-12 11:31:30 +0200

    on ne modifie plus les prix

--- src/assets/scripts/pizza-size.js
+++ src/assets/scripts/pizza-size.js
@@ -1,7 +1,7 @@
 let pizzaSizeButtons = document.getElementById("app-pizza-size").children;
 let price = document.getElementById("app-price");
 
-let tabPrice = ["12.00", "13.80", "14.90"];
+// let tabPrice = ["12.00", "13.80", "14.90"];
 
 for (let i = 0; i < pizzaSizeButtons.length; i++) {
   pizzaSizeButtons[i].addEventListener("click", chooseSize);
@@ -12,7 +12,7 @@ function chooseSize() {
     pizzaSizeButtons[i].classList.remove("active");
   }
   this.classList.add("active");
-  changePrice(this.textContent);
+  // changePrice(this.textContent);
 }
 
 function changePrice(value) {
