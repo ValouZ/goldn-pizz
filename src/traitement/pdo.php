98f9fbeb81f3d03c74a802c1e527988a394220bb
ValouZ <debray.va@gmail.com>
2021-04-12 11:36:27 +0200

    modif pdo en ligne

--- src/traitement/pdo.php
+++ src/traitement/pdo.php
@@ -1,12 +1,23 @@
 <?php
 $option = [
-  PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', 
+  PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 ];
 
 try {
-  $bdd = new PDO('mysql:host=localhost;dbname=goldnpizz;','root','', $option);
-}catch(PDOException $e){
-  echo 'Echeec lors de la connexion' . $e->getMessage(); 
+  $bdd = new PDO('mysql:host=localhost;dbname=goldnpizz;', 'root', '', $option);
+} catch (PDOException $e) {
+  echo 'Echeec lors de la connexion' . $e->getMessage();
 }
-?>
+
+// Pour le site en ligne
+// $option = [
+//   PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', 
+//   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+// ];
+
+// try {
+//   $bdd = new PDO('mysql:host=localhost;dbname=wugkyutw_goldnpizz;','wugkyutw_goldnpizz','Moliku36!?', $option);
+// }catch(PDOException $e){
+//   echo 'Echeec lors de la connexion' . $e->getMessage(); 
+// }
