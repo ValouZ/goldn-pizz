c6666140a92f45cd1338a81bbf1fa1c3011a51d2
Sopralus <54934553+Sopralus@users.noreply.github.com>
2021-04-14 00:36:53 +0200

    commentaire admin

--- src/assets/scripts/update-admin.js
+++ src/assets/scripts/update-admin.js
@@ -1,6 +1,11 @@
+
+// On récupère le bouton update
 let updateButton = document.getElementsByClassName("button__update");
+// Création d'une variable pour la longueur du bouton updatebutton
 let updateButtonLength = updateButton.length;
 
+
+// Boucle qui permet d'ajouter des eventListener sur les boutons 
 for (let i = 0; i < updateButtonLength; i++) {
   updateButton[i].addEventListener("click", function (e) {
     e.preventDefault();
@@ -8,23 +13,31 @@ for (let i = 0; i < updateButtonLength; i++) {
   });
 }
 
+// Fonction pour mettre a jour chaque element de notre tableau admin
 function updateAdmin(button) {
+  // Variable pour stocker une ligne de tableau
   let line = button.parentNode.parentNode;
+  // On récupère l'élément exact du nomPizza
   let nomPizza = line.children[1].children[0];
+  // On récupère l'élément exact du prixPizza
   let prixPizza = line.children[2].children[0];
+  // On récupère l'élément exact de descriPizza
   let descriPizza = line.children[3].children[0];
+  // On récupère l'élément exact du button valider
   let validateButton = line.children[5].children[0];
 
   disableUpdate();
-  console.log(validateButton);
-  // console.log(nomPizza);
 
+  // On débloque tout les inputs de la ligne
   prixPizza.disabled = false;
   nomPizza.disabled = false;
+  descriPizza.disabled = false;
 
+  // On affiche le bouton de validation
   validateButton.classList.remove("hide");
 }
 
+// Fonction permettant de désactiver tout les boutons de modifications
 function disableUpdate() {
   for (let i = 0; i < updateButtonLength; i++) {
     updateButton[i].disabled = true;
--- src/traitement/update-admin.php
+++ src/traitement/update-admin.php
@@ -1,26 +1,32 @@
 <?php
 
 session_start();
+//Inclusion de pdo
 include('pdo.php');
 
-var_dump($_POST);
-
+// Si le bouton valider est cliqué
 if (isset($_POST['validate'])) {
+  
   
   $nomPizza = htmlspecialchars($_POST['nom-pizza']);
   $prixPizza = htmlspecialchars($_POST['prix-pizza']);
   $idPizza = htmlspecialchars($_POST['id-pizza']);
   $descriPizza = htmlspecialchars($_POST['descri-pizza']);
-  var_dump($descriPizza);
 
+   // On vérife que tous les champs soient bien remplis
   if (!empty($_POST['nom-pizza']) AND !empty($_POST['prix-pizza']) AND !empty($_POST['descri-pizza'])) {
+    // Vérification que le prix de la pizza soit bien en float
     if (filter_var($prixPizza, FILTER_VALIDATE_FLOAT)) {
+      // Requête permettant de mettre a jour la table pizza ou l'id pizza est celle que l'on a selectionné
       $req = $bdd->prepare('UPDATE pizza SET nom_pizza = ?, prix_pizza = ?, description_pizza = ? WHERE id_pizza = ?');
+      // On execute la requête
       $req->execute(array($nomPizza, $prixPizza,$descriPizza, $idPizza));
+      // Redirection vers la page admin
       header('location:../admin.php');
       exit();
     }
   }
 } else {
-  // header('location:../admin.php?error=0');
+  // Redirection vers la page admin en affichant une erreur
+  header('location:../admin.php?error=0');
 }
