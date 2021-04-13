67d139f8d049fc0c4752f931724a386322fffa27
ValouZ <debray.va@gmail.com>
2021-04-14 00:14:18 +0200

    suppression parametre

--- src/assets/scripts/update-admin.js
+++ src/assets/scripts/update-admin.js
@@ -1,18 +1,14 @@
 let updateButton = document.getElementsByClassName("button__update");
-let updateButtonLength =updateButton.length;
+let updateButtonLength = updateButton.length;
 
 for (let i = 0; i < updateButtonLength; i++) {
   updateButton[i].addEventListener("click", function (e) {
     e.preventDefault();
     updateAdmin(this);
   });
-
-  
 }
 
-
-function updateAdmin(button){
-
+function updateAdmin(button) {
   let line = button.parentNode.parentNode;
   let nomPizza = line.children[1].children[0];
   let prixPizza = line.children[2].children[0];
@@ -28,11 +24,8 @@ function updateAdmin(button){
   validateButton.classList.remove("hide");
 }
 
-function disabledUpdate(button){
-
-  for(let i = 0; i< updateButtonLength; i++){
-    updateButton[i].disabled=true;
-    
+function disabledUpdate() {
+  for (let i = 0; i < updateButtonLength; i++) {
+    updateButton[i].disabled = true;
   }
-
 }
